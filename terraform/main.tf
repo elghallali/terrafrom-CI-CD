8df42e8852e8362c539615bbc5d815cac12584cf
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-11-04 15:15:44 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "myEC2" {
