4665d280d05176fe9cedb53c590052648225126d
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-11-04 11:38:29 +0100

    update terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-module "ec2module" {
-  source = "./mod"
-
+resource "aws_instance" "myEC2" {
+  ami = "ami-0dbc3d7bc646e8516"
+  instance_type = "t2.micro"
 }
-
--- terraform/mod/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "myEC2" {
-  ami = "ami-0dbc3d7bc646e8516"
-  instance_type = "t2.micro"
-  count = 3
-}
