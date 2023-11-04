ca840b51bde2daf2df95d30cebc360edc9fc57f7
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-11-04 12:09:37 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "myEC2" {
-  ami = "ami-0dbc3d7bc646e8516"
+  ami           = "ami-0dbc3d7bc646e8516"
   instance_type = "t2.micro"
 }
